f4c50262953b893a43d5cc8894162eb0eacbfe79
Jody <koenig6@byui.edu>
2019-09-25 16:22:12 -0400

    starting array

--- js/loopCondiionFunArray.js
+++ js/loopCondiionFunArray.js
@@ -4,18 +4,77 @@ function evenlyDivisible (){
  var divisor = parseInt(document.getElementById("divisor").value);
  var output = "";
 
- for (var counter = start ; counter <= end; counter++)
-		{
+for (var counter = start ; counter <= end; counter++)
+    {
 
-			if (( counter % divisor) == 0 ){
-		output += counter + " ";
-	}
+        if (( counter % divisor) == 0 )
+            {
+		      output += counter + " ";
+	       }
 
-	else{
-		output = "No numbers are divisible by your divisor.";
-	}
+	   else
+       {
+		  output = "No numbers are divisible by your divisor.";
+	   }
 
 
-  }
+    }
  document.getElementById('output').innerHTML = output
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    function switchItup(){
+	var size = parseInt(document.getElementById("sizeOf").value);
+
+    var theList = [];
+
+    for(var i = 0; i < size; i++)
+        {
+            theList.push(prompt"Enter a number");
+
+        }
+
+
+/*var theList = [1, 4, 2, 7, 9];*/
+
+	var output = "Your numbers flipped will be " + reverseList(theList);
+
+  document.getElementById('outputDiv').innerHTML = output;
+
+}
+
+
+
+function reverseList(theList){
+
+	var retval = [];
+
+	for (move = theList.length-1; move >= 0; move--)
+	{
+		retval.push (theList [move]);
+		{
+		output = move;
+		}
+
+	}
+	//this makes the first number the last number
+	//var swop = theList[0] = theList[theList.length -1];
+	return retval;
+}
+
  }
+
