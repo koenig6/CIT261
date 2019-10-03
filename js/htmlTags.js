71ae4462db8dcc1861fce4c2cbd9b382c2a32cae
Jody <koenig6@byui.edu>
2019-10-03 19:57:55 -0400

    trying to get canvas to work

--- js/htmlTags.js
+++ js/htmlTags.js
@@ -1,5 +1,5 @@
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext("2d");
-ctx.font = "20px Ariel";
+ctx.font = "20px Arial";
 ctx.fillStyle = "black";
-ctx.fillText("Welcome to the Zoo", 75, 200);
+ctx.fillText("Welcome to the Zoo", 200, 200);
