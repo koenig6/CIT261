d46b24509851fc841c7a09eb2562c787445cf03d
Jody <koenig6@byui.edu>
2019-10-03 20:14:23 -0400

    adjusting canvas

--- js/htmlTags.js
+++ js/htmlTags.js
@@ -2,4 +2,4 @@ var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext("2d");
 ctx.font = "20px Arial";
 ctx.fillStyle = "black";
-ctx.fillText("Welcome to the Zoo", 200, 200);
+ctx.fillText("Welcome to the Zoo", 65, 75);
