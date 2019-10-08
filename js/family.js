e86450db34ae1b21f0a24b406329282a244757ab
Jody <koenig6@byui.edu>
2019-10-08 17:36:21 -0400

    testing js

--- js/family.js
+++ js/family.js
@@ -1,17 +1,16 @@
-document.getElementsByTagName('article').onmouseover = function()
-{mouseOver(this)};
+document.getElementsByTagName('article').addEventListener('mouseover', mouseOver);
 
 document.getElementsByTagName('article').onmouseout = function()
 {mouseOut(this)};
 
-function mouseOver(x) {
-  document.getElementsByTagName("article").style.height = "330px";
+function mouseOver() {
+  document.getElementsByTagName('article').style.height = "330px";
    document.getElementsByTagName('article').style.width = "283.99px";
      document.getElementsByTagName("article").style.color = "#3AAFA9";
 }
 
 function mouseOut(x) {
-  document.getElementsByTagName("article").style.height = "264px";
+  document.getElementsByTagName('article').style.height = "264px";
    document.getElementsByTagName('article').style.width = "227.19px"
     document.getElementsByTagName("article").style.color = "black";
 }
