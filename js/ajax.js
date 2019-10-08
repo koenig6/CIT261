f95f1d5ed4a0d19400dea2c9226b4a51469eaff5
Jody <koenig6@byui.edu>
2019-10-08 13:43:04 -0400

    test

--- js/ajax.js
+++ js/ajax.js
@@ -1,4 +1,4 @@
-var data = null;
+/*var data = null;
 
 var loveObject = new XMLHttpRequest();
 
@@ -18,3 +18,21 @@ loveObject.onclick = function(){
     document.getElementById('percentage').innerHTML = loveInfo.percentage;
     document.getElementById('result').innerHTML = loveInfo.result;
 }
+*/
+
+var data = null;
+
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+	if (this.readyState === this.DONE) {
+		console.log(this.responseText);
+	}
+});
+
+xhr.open("GET", "https://love-calculator.p.rapidapi.com/getPercentage?fname=John&sname=Alice");
+xhr.setRequestHeader("x-rapidapi-host", "love-calculator.p.rapidapi.com");
+xhr.setRequestHeader("x-rapidapi-key", "b086763a6fmshd30cfe176b59170p1721d3jsn383d0383abde");
+
+xhr.send(data);
