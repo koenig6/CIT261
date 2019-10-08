9c15612558ccd764fcfdf7cd734118aaf136ca34
Jody <koenig6@byui.edu>
2019-10-08 17:10:57 -0400

    changing color on mouse over

--- js/family.js
+++ js/family.js
@@ -7,9 +7,11 @@ document.getElementsByTagName('article').onmouseout = function()
 function mouseOver() {
   document.getElementsByTagName("article").style.height = "330px";
    document.getElementsByTagName('article').style.width = "283.99px";
+     document.getElementsByTagName("article").style.color = "#3AAFA9";
 }
 
 function mouseOut() {
   document.getElementsByTagName("article").style.height = "264px";
    document.getElementsByTagName('article').style.width = "227.19px"
+    document.getElementsByTagName("article").style.color = "black";
 }
