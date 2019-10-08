a18668b80cde4b15f2b648b0746cfa1bee25e384
Jody <koenig6@byui.edu>
2019-10-07 21:53:26 -0400

    trying to get ajax to work

--- js/ajax.js
+++ js/ajax.js
@@ -1,18 +1,18 @@
-var data = null;
-
-var xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
-
-xhr.addEventListener("readystatechange", function () {
-	if (this.readyState === this.DONE) {
-		console.log(this.responseText);
-	}
-});
-
-xhr.open("GET", "https://love-calculator.p.rapidapi.com/getPercentage?fname=%3Crequired%3E&sname=%3Crequired%3E");
-xhr.setRequestHeader("x-rapidapi-host", "love-calculator.p.rapidapi.com");
-xhr.setRequestHeader("x-rapidapi-key", "b086763a6fmshd30cfe176b59170p1721d3jsn383d0383abde");
-
-xhr.send(data);
 
 
+var loveObject = new XMLHttpRequest();
+
+loveObject.open("GET", "https://love-calculator.p.rapidapi.com/getPercentage?fname=fname&sname=sname");
+
+loveObject.send();
+
+loveObject.onclick = function(){
+
+    var loveInfo = JSON.parse(loveObject.responseText)
+    console.log(loveInfo);
+
+    document.getElementById('fname').innerHTML = loveInfo.fname;
+    document.getElementById('sname').innerHTML = loveInfo.sname;
+    document.getElementById('percentage').innerHTML = loveInfo.percentage;
+    document.getElementById('result').innerHTML = loveInfo.result;
+}
