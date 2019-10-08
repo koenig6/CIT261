833349caf9da9bfc657880e5ab63a4140baa4ad2
Jody <koenig6@byui.edu>
2019-10-08 17:23:05 -0400

    fixing mouseOver

--- js/family.js
+++ js/family.js
@@ -1,16 +1,16 @@
 document.getElementsByTagName('article').onmouseover = function()
-{mouseOver()};
+{mouseOver(this)};
 
 document.getElementsByTagName('article').onmouseout = function()
-{mouseOut()};
+{mouseOut(this)};
 
-function mouseOver() {
+function mouseOver(x) {
   document.getElementsByTagName("article").style.height = "330px";
    document.getElementsByTagName('article').style.width = "283.99px";
      document.getElementsByTagName("article").style.color = "#3AAFA9";
 }
 
-function mouseOut() {
+function mouseOut(x) {
   document.getElementsByTagName("article").style.height = "264px";
    document.getElementsByTagName('article').style.width = "227.19px"
     document.getElementsByTagName("article").style.color = "black";
