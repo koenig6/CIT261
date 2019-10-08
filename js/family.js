7d23c90ebb93fe0573052deeeb0f7094dbcd2a4a
Jody <koenig6@byui.edu>
2019-10-08 17:40:00 -0400

    typo?

--- js/family.js
+++ js/family.js
@@ -1,16 +1,16 @@
-document.getElementsByTagName('article').addEventListener('mouseover', mouseOver);
+document.getElementByTagName('article').addEventListener('mouseover', mouseOver);
 
-document.getElementsByTagName('article').onmouseout = function()
+document.getElementByTagName('article').onmouseout = function()
 {mouseOut(this)};
 
 function mouseOver() {
-  document.getElementsByTagName('article').style.height = "330px";
-   document.getElementsByTagName('article').style.width = "283.99px";
-     document.getElementsByTagName("article").style.color = "#3AAFA9";
+  document.getElementByTagName('article').style.height = "330px";
+   document.getElementByTagName('article').style.width = "283.99px";
+     document.getElementByTagName("article").style.color = "#3AAFA9";
 }
 
 function mouseOut(x) {
-  document.getElementsByTagName('article').style.height = "264px";
-   document.getElementsByTagName('article').style.width = "227.19px"
-    document.getElementsByTagName("article").style.color = "black";
+  document.getElementByTagName('article').style.height = "264px";
+   document.getElementByTagName('article').style.width = "227.19px"
+    document.getElementByTagName("article").style.color = "black";
 }
