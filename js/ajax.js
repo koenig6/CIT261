d8184deb3af776b6f7cc52d6d78af6eb5e53d68e
Jody <koenig6@byui.edu>
2019-10-12 17:26:08 -0400

    cleaning up code

--- js/ajax.js
+++ js/ajax.js
@@ -3,14 +3,13 @@ var jokeData = null;
 function getData(){
 var xhr = new XMLHttpRequest();
 
-
 xhr.addEventListener("readystatechange", function () {
 	if (this.readyState === this.DONE) {
 		saveToLocal(this.responseText);
 	}
 });
 
-xhr.open("GET", "https://sv443.net/jokeapi/category/Programming"); //document.getElementById("category_name").value);
+xhr.open("GET", "https://sv443.net/jokeapi/category/Programming");
 xhr.setRequestHeader("x-rapidapi-host", "jokeapi.p.rapidapi.com");
 xhr.setRequestHeader("x-rapidapi-key", "b086763a6fmshd30cfe176b59170p1721d3jsn383d0383abde");
 
@@ -52,15 +51,62 @@ function getJoke() {
 
 
 
+var data = null;
+
+function getFoodData(){
+
+var xhr = new XMLHttpRequest();
+
+xhr.addEventListener("readystatechange", function () {
+	if (this.readyState === this.DONE) {
+		saveToFoodLocal(this.responseText);
+	}
+});
+
+xhr.open("GET", "https://food-calorie-data-search.p.rapidapi.com/api/search?keyword=" + document.getElementById("search").value);
 
 
+xhr.setRequestHeader("x-rapidapi-host", "food-calorie-data-search.p.rapidapi.com");
+xhr.setRequestHeader("x-rapidapi-key", "b086763a6fmshd30cfe176b59170p1721d3jsn383d0383abde");
+
+xhr.send(data);
+}
 
 
+function saveToFoodLocal(response) {
+
+	localStorage.clear();
+
+    localStorage.setItem("Food", response);
+
+}
 
 
+function displayResults() {
 
+    var foodItems = localStorage.getItem("Food");
 
+   data = JSON.parse(foodItems);
 
+    //this resets the screen
+    document.getElementById("foodResults").innerHTML = "";
+
+    for(var a = 0; a < data.length; a++)
+        {
+            document.getElementById("foodResults").innerHTML += "Food: " + data[a].shrt_desc + "<br>";
+
+            document.getElementById("foodResults").innerHTML += "Calories: " + data[a].energ_kcal + "<br>";
+
+            document.getElementById("foodResults").innerHTML += "Carbohydrates: " + data[a].carbohydrt + "<br>";
+
+            document.getElementById("foodResults").innerHTML += "Protein: " + data[a].protein + "<br>";
+        }
+}
+
+function foodMatch() {
+    getFoodData();
+    displayResults();
+  }
 
 
 
