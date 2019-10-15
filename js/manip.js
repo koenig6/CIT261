72453b85a053975d0cc87e8a15f414b60bb26e3c
Jody <koenig6@byui.edu>
2019-10-15 18:14:18 -0400

    clearing excess code

--- js/manip.js
+++ js/manip.js
@@ -3,7 +3,7 @@ function changeBackgroundColor() {
 	var textbox_id = "txtColor";
 	var textbox = document.getElementById(textbox_id);
 
-	var div_id = "div1";
+	var div_id = "div2";
 	var div = document.getElementById(div_id);
 
 	// We should verify values here before we use them...
@@ -35,5 +35,31 @@ function changeTextColor() {
 	// We should verify values here before we use them...
 	var color = textbox.value;
 	div.style.color = color;
+
+}
+
+function changeFont() {
+	var textbox_id = "font";
+	var textbox = document.getElementById(textbox_id);
+
+	var div_id = "div2";
+	var div = document.getElementById(div_id);
+
+	// We should verify values here before we use them...
+	var font = textbox.value;
+	div.style.fontFamily = font;
+
+}
+
+function changeWidth() {
+	var textbox_id = "width";
+	var textbox = document.getElementById(textbox_id);
+
+	var div_id = "div2";
+	var div = document.getElementById(div_id);
+
+	// We should verify values here before we use them...
+	var width = textbox.value;
+	div.style.width = width;
 
 }
