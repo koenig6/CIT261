d8759a679d81a2703ae6bb749a73173c24c0ad4f
Jody <koenig6@byui.edu>
2019-11-12 18:32:28 -0500

    update

---
+++ finalProject/final.js
@@ -0,0 +1,29 @@
+function cupcakes()
+{
+    document.getElementById("one").innerHTML="<img src='img/andes.jpg' height=200px width=200px />";
+
+    document.getElementById("two").innerHTML="<img src='img/bear.jpg' height=200px width=200px />";
+
+    document.getElementById("three").innerHTML="<img src='img/monster.jpg' height=200px width=200px />";
+
+}
+
+function cookies()
+{
+    document.getElementById("one").innerHTML="<img src='img/hearts.jpg' height=200px width=200px />";
+
+    document.getElementById("two").innerHTML="<img src='img/onsie.jpg' height=200px width=200px />";
+
+    document.getElementById("three").innerHTML="<img src='img/paint.jpg' height=200px width=200px />";
+
+}
+
+function cakes()
+{
+    document.getElementById("one").innerHTML="<img src='img/marbeling.jpg' height=200px width=200px />";
+
+    document.getElementById("two").innerHTML="<img src='img/ruffles.jpg' height=200px width=200px />";
+
+    document.getElementById("three").innerHTML="<img src='img/topFlower.jpg' height=200px width=200px />";
+
+}
