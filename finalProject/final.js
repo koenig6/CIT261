03df66aa67389aac863736d19257765d338c96ff
Jody <koenig6@byui.edu>
2019-12-05 13:52:55 -0500

    update

--- finalProject/final.js
+++ finalProject/final.js
@@ -27,3 +27,46 @@ function cakes()
     document.getElementById("three").innerHTML="<img src='img/topFlower.jpg' height=200px width=200px />";
 
 }
+
+function addItem()
+{
+    var ul = document.getElementById("myList");
+    var newItem = document.getElementById("item"); //get text from box
+    var li = document.createElement("li"); // create li
+    li.setAttribute('id',item.value);
+    li.appendChild(document.createTextNode(item.value)); // add to list
+    ul.appendChild(li); // add to list
+}
+
+
+function removeItem()
+{
+    var ul = document.getElementById("myList");
+    var item = document.getElementById("item");
+    var li = document.getElementById(item.value);
+    ul.removeChild(li);
+}
+
+function animationOne()
+{
+    if (document.getElementById('animationOne'))
+        {
+           var color = document.getElementById('animationOne');
+            color.style.transition = "background-color 1.5s linear 0s"; // short hand target, duration to occur, timing, delay
+            color.style.background = "orchid";
+            color.addEventListener("transitionend", function(){
+                color.style.background="rgba(255, 255, 255, 0.8)";
+            });
+        }
+    else if (document.getElementById('animationThree'))
+        {
+            var color = document.getElementById('animationThree');
+            color.style.transition = "background-color 1.5s linear 0s"; // short hand target, duration to occur, timing, delay
+            color.style.background = "orchid";
+            color.addEventListener("transitionend", function(){
+                color.style.background="rgba(255, 255, 255, 0.8)";
+            });
+        }
+
+
+}
