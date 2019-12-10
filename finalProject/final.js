43466c83b6c008d81c27688fffdb296433e7f391
Jody <koenig6@byui.edu>
2019-12-10 09:35:20 -0500

    update

--- finalProject/final.js
+++ finalProject/final.js
@@ -82,23 +82,17 @@ function removeItem()
 // Create a new color picker instance
 // https://iro.js.org/guide.html#getting-started
 var colorPicker = new iro.ColorPicker(".colorPicker", {
-  // color picker options
-  // Option guide: https://iro.js.org/guide.html#color-picker-options
-  width: 280,
-  color: "rgb(255, 0, 0)",
+  width: 180, // the size of the colorpicker
+  color: "rgb(255, 0, 0)", //initial color on load
   borderWidth: 1,
-  borderColor: "#fff",
+  borderColor: "black",
 });
 
 var values = document.getElementById("values");
 
-// https://iro.js.org/guide.html#color-picker-events
 colorPicker.on(["color:init", "color:change"], function(color){
-  // Show the current color in different formats
-  // Using the selected color: https://iro.js.org/guide.html#selected-color-api
   values.innerHTML = [
     "hex: " + color.hexString,
     "rgb: " + color.rgbString,
-    "hsl: " + color.hslString,
   ].join("<br>");
 });
