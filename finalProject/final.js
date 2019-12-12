cf00874af5156b17fe307c038e59c0a3d37b5fa2
Jody <koenig6@byui.edu>
2019-12-11 22:01:43 -0500

    t

--- finalProject/final.js
+++ finalProject/final.js
@@ -2,15 +2,15 @@ function cupcakes()
 {
     document.getElementById("1").innerHTML="<img src='img/andes.jpg' height=200px width=200px />";
 
-    document.getElementById("two").innerHTML="<img src='img/bear.jpg' height=200px width=200px />";
+    document.getElementById("2").innerHTML="<img src='img/bear.jpg' height=200px width=200px />";
 
-    document.getElementById("three").innerHTML="<img src='img/apple.jpg' height=200px width=200px />";
+    document.getElementById("3").innerHTML="<img src='img/apple.jpg' height=200px width=200px />";
 
-     document.getElementById("four").innerHTML="<img src='img/smores.jpg' height=200px width=200px />";
+     document.getElementById("4").innerHTML="<img src='img/smores.jpg' height=200px width=200px />";
 
-     document.getElementById("five").innerHTML="<img src='img/cookieMonster.jpg' height=200px width=200px />";
+     document.getElementById("5").innerHTML="<img src='img/cookieMonster.jpg' height=200px width=200px />";
 
-     document.getElementById("six").innerHTML="<img src='img/purple.jpg' height=200px width=200px />";
+     document.getElementById("6").innerHTML="<img src='img/purple.jpg' height=200px width=200px />";
 
 }
 
@@ -18,15 +18,15 @@ function cookies()
 {
     document.getElementById("1").innerHTML="<img src='img/hearts.jpg' height=200px width=200px />";
 
-    document.getElementById("two").innerHTML="<img src='img/onsie.jpg' height=200px width=200px />";
+    document.getElementById("2").innerHTML="<img src='img/onsie.jpg' height=200px width=200px />";
 
-    document.getElementById("three").innerHTML="<img src='img/paint.jpg' height=200px width=200px />";
+    document.getElementById("3").innerHTML="<img src='img/paint.jpg' height=200px width=200px />";
 
-     document.getElementById("four").innerHTML="<img src='img/lightbulb.jpg' height=200px width=200px />";
+     document.getElementById("4").innerHTML="<img src='img/lightbulb.jpg' height=200px width=200px />";
 
-     document.getElementById("five").innerHTML="<img src='img/dress.jpg' height=200px width=200px />";
+     document.getElementById("5").innerHTML="<img src='img/dress.jpg' height=200px width=200px />";
 
-     document.getElementById("six").innerHTML="<img src='img/watermelon.jpg' height=200px width=200px />";
+     document.getElementById("6").innerHTML="<img src='img/watermelon.jpg' height=200px width=200px />";
 
 }
 
@@ -34,15 +34,15 @@ function cakes()
 {
     document.getElementById("1").innerHTML="<img src='img/marbeling.jpg' height=200px width=200px />";
 
-    document.getElementById("two").innerHTML="<img src='img/hat.jpg' height=200px width=200px />";
+    document.getElementById("2").innerHTML="<img src='img/hat.jpg' height=200px width=200px />";
 
-    document.getElementById("three").innerHTML="<img src='img/unicorn.jpg' height=200px width=200px />";
+    document.getElementById("3").innerHTML="<img src='img/unicorn.jpg' height=200px width=200px />";
 
-     document.getElementById("four").innerHTML="<img src='img/ruffles.jpg' height=200px width=200px />";
+     document.getElementById("4").innerHTML="<img src='img/ruffles.jpg' height=200px width=200px />";
 
-     document.getElementById("five").innerHTML="<img src='img/starwars.jpg' height=200px width=200px />";
+     document.getElementById("5").innerHTML="<img src='img/starwars.jpg' height=200px width=200px />";
 
-     document.getElementById("six").innerHTML="<img src='img/trolls.jpg' height=200px width=200px />";
+     document.getElementById("6").innerHTML="<img src='img/trolls.jpg' height=200px width=200px />";
 
 }
 
@@ -50,15 +50,15 @@ function price()
 {
      document.getElementById("1").innerHTML="6\" Cake (12 servings) - $35<br>8\" Cake (20 servings) - $60<br>10\" Cake (28 servings) -$85<br>12\" Cake (40 servings) - $120<br>Half Sheet Cake (24 servings) - $72<br>Full Sheet Cake (54 Servings) - $150<br>Extra $50 for Carved Cakes ";
 
-     document.getElementById("two").innerHTML="Basic Cupcakes  - $2.00 per cupcake<br>Themed Decorated Cupcakes or Toppers - $2.25 per cupcake<br><br>Specialty Cupcakes - $2.50 per cupcake (includes fillings or Cookies and Cream Cupcake, Chocolate Chip Cookie Dough cupcake, German Chocolate Cupcakes, etc.)";
+     document.getElementById("2").innerHTML="Basic Cupcakes  - $2.00 per cupcake<br>Themed Decorated Cupcakes or Toppers - $2.25 per cupcake<br><br>Specialty Cupcakes - $2.50 per cupcake (includes fillings or Cookies and Cream Cupcake, Chocolate Chip Cookie Dough cupcake, German Chocolate Cupcakes, etc.)";
 
-    document.getElementById("three").innerHTML="Mini Cookies - $1.50 per Cookie<br>2-3 Colors - $3.00 per Cookie<br>4+ Colors - $3.20 per Cookie";
+    document.getElementById("3").innerHTML="Mini Cookies - $1.50 per Cookie<br>2-3 Colors - $3.00 per Cookie<br>4+ Colors - $3.20 per Cookie";
 
-     document.getElementById("four").innerHTML=" ";
+     document.getElementById("4").innerHTML=" ";
 
-    document.getElementById("four").innerHTML=" ";
+    document.getElementById("5").innerHTML=" ";
 
-    document.getElementById("four").innerHTML=" ";
+    document.getElementById("6").innerHTML=" ";
 }
 
 function addItem()
